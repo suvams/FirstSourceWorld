1f0a33f5d9ab9e2ecbc8fbfef3ced6df1beea16e
nirajan1211 <nirajan.shahi121@gmail.com>
2023-05-17 18:00:48 +0545

    Refactor

--- src/screens/example.js
+++ src/screens/example.js
@@ -1,149 +1,86 @@
-import React, { useState } from "react";
-import { View, Text, Button, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Modal, View, Text, TouchableOpacity, Animated } from "react-native";
 
-const data = [
-  {
-    id: 1,
-    location: "Location 1",
-    subLocation: "Sub Location 1",
-    name: "Item 1",
-  },
-  {
-    id: 2,
-    location: "Location 2",
-    subLocation: "Sub Location 1",
-    name: "Item 2",
-  },
-  {
-    id: 3,
-    location: "Location 1",
-    subLocation: "Sub Location 2",
-    name: "Item 3",
-  },
-  {
-    id: 4,
-    location: "Location 3",
-    subLocation: "Sub Location 2",
-    name: "Item 4",
-  },
-  {
-    id: 5,
-    location: "Location 2",
-    subLocation: "Sub Location 1",
-    name: "Item 5",
-  },
-  // Add more data items as needed
-];
+const SlideModal = ({ visible, onClose }) => {
+  const [slideAnim] = useState(new Animated.Value(0));
 
-const HomeScreen = () => {
-  const [filteredData, setFilteredData] = useState(data);
-  const [selectedLocation, setSelectedLocation] = useState(null);
-  const [selectedSubLocation, setSelectedSubLocation] = useState(null);
-
-  const handleLocationFilter = () => {
-    setFilteredData(data);
-    setSelectedLocation(null);
-    setSelectedSubLocation(null);
-  };
-
-  const handleSubLocationFilter = () => {
-    setFilteredData(data);
-    setSelectedSubLocation(null);
-  };
-
-  const renderItem = ({ item }) => (
-    <View style={{ marginBottom: 10 }}>
-      <Text>{item.name}</Text>
-    </View>
-  );
-
-  const filteredByLocation = selectedLocation
-    ? data.filter((item) => item.location === selectedLocation)
-    : data;
-
-  const filteredBySubLocation = selectedSubLocation
-    ? filteredByLocation.filter(
-        (item) => item.subLocation === selectedSubLocation
-      )
-    : filteredByLocation;
+  useEffect(() => {
+    Animated.timing(slideAnim, {
+      toValue: visible ? 1 : 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }, [visible]);
 
   return (
-    <View>
-      <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}>
-        Filter by Location:
-      </Text>
-      <View style={{ flexDirection: "row", marginBottom: 10 }}>
-        {selectedLocation && (
-          <Button
-            title="Remove Location Filter"
-            onPress={handleLocationFilter}
-            color="red"
-          />
-        )}
-        {!selectedLocation && (
-          <>
-            <Button
-              title="Location 1"
-              onPress={() => setSelectedLocation("Location 1")}
-            />
-            <Button
-              title="Location 2"
-              onPress={() => setSelectedLocation("Location 2")}
-            />
-            <Button
-              title="Location 3"
-              onPress={() => setSelectedLocation("Location 3")}
-            />
-            {/* Add more location buttons as needed */}
-          </>
-        )}
-      </View>
-
-      {selectedLocation && (
-        <>
-          <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}>
-            Filter by Sub Location:
-          </Text>
-          <View style={{ flexDirection: "row", marginBottom: 10 }}>
-            {selectedSubLocation && (
-              <Button
-                title="Remove Sub Location Filter"
-                onPress={handleSubLocationFilter}
-                color="red"
-              />
-            )}
-            {!selectedSubLocation && (
-              <>
-                <Button
-                  title="Sub Location 1"
-                  onPress={() => setSelectedSubLocation("Sub Location 1")}
-                />
-                <Button
-                  title="Sub Location 2"
-                  onPress={() => setSelectedSubLocation("Sub Location 2")}
-                />
-                {/* Add more sublocation buttons as needed */}
-              </>
-            )}
-          </View>
-
-          <FlatList
-            data={filteredBySubLocation}
-            renderItem={renderItem}
-            keyExtractor={(item) => item.id.toString()}
-          />
-        </>
-      )}
-
-      {!selectedLocation && !selectedSubLocation && (
-        <FlatList
-          data={filteredData}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id.toString()}
-        />
-      )}
-    </View>
+    <Modal
+      visible={visible}
+      transparent={true}
+      animationType="none"
+      onRequestClose={onClose}
+    >
+      <TouchableOpacity style={{ flex: 1, marginTop: 80 }} onPress={onClose}>
+        <Animated.View
+          style={{
+            flex: 1,
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            transform: [
+              {
+                translateY: slideAnim.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [600, 0],
+                }),
+              },
+            ],
+            justifyContent: "center",
+            // alignItems: "center",
+          }}
+        >
+          <Animated.View
+            style={{
+              backgroundColor: "white",
+              padding: 20,
+              borderRadius: 10,
+              opacity: slideAnim.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, 1],
+              }),
+              transform: [
+                {
+                  translateY: slideAnim.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [50, 0],
+                  }),
+                },
+              ],
+            }}
+          >
+            <Text>Modal content goes here</Text>
+          </Animated.View>
+        </Animated.View>
+      </TouchableOpacity>
+    </Modal>
   );
 };
 
-export default HomeScreen;
+export default function App() {
+  const [visible, setVisible] = useState(false);
+
+  const handleOpen = () => {
+    setVisible(true);
+  };
+
+  const handleClose = () => {
+    setVisible(false);
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <TouchableOpacity onPress={handleOpen}>
+        <Text>Open Modal</Text>
+      </TouchableOpacity>
+
+      <SlideModal visible={visible} onClose={handleClose} />
+    </View>
+  );
+}
